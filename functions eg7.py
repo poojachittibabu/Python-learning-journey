e4e11a25fd487e01d5732b9000d038305102c078
Pooja C <poojac0307@gmail.com>
2026-07-08 19:37:33 +0530

    functions eg7.py learned

---
+++ functions eg7.py
@@ -0,0 +1,12 @@
+def findpassorfail(mark):
+    if(mark>35):
+        print("pass")
+    else:
+        print("fail")
+    if(mark%2==0):
+        print("even")
+    else:
+        print("odd")
+a=int(input())
+findpassorfail(a)
+
